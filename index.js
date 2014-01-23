935006b88f253262be89740d23b79346de678bc8
Thomas BRELET <tbrelet@wyplay.com>
2014-01-23 16:43:42 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.configure(function () {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+});
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/static'));
+});
+
+app.get('/nom', function(req, res){
+  noms = ["Jean Aimar", "Ella Faim", "Amer Credi"];
+  res.send('{"nom": "' + noms[Math.floor(Math.random() * noms.length)] + '"}');
+});
+
+app.listen(3000);
