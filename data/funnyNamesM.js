85e011cfb376a89e86d8a248bb47c1feada9d835
Thomas Brelet <thomas.brelet@gmail.com>
2014-01-29 16:41:41 +0100

    Update funnyNamesM.js

--- data/funnyNamesM.js
+++ data/funnyNamesM.js
@@ -1,4 +1,17 @@
 module.exports = [
-	"Jean Aimar",
-	"Amer Credi"
+	"Amer CREDI",
+	"Alain DECY",
+	"Axel AIRE",
+	"Denis DIRON-DAYLE",
+	"Géo GRAFF",
+	"Jacques CELAIR",
+	"Jean AIMAR",
+	"John DEUFF",
+	"José PALDIR",
+	"Laurent DEVOUX",
+	"Louis FINE",
+	"Luc SURIEUX
+	"Marc ASSIN",
+	"Régis CIDE",
+	"Xavier KAFAIRGAF",
 ];
