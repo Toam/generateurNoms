66b061840b6544e567db7049753cb083a1d7870b
Thomas BRELET <thomas.brelet@gmail.com>
2014-01-30 22:19:38 +0100

    oups

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 var getName = function getName () {
   $("#generate").text("Chargement ...");
   $("#generate").addClass("disabled");
-  ga('send', 'event', 'requete', 'nom', $('#funny').is(":checked");
+  ga('send', 'event', 'requete', 'nom', $('#funny').is(":checked"));
   $.ajax({
     type: 'GET',
     url: 'http://gen.toam.fr/name',
